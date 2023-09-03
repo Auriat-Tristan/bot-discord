beb4341eaeaf3d615271aa7626fd08ae836fcfd0
Auriat-Tristan <128914135+Auriat-Tristan@users.noreply.github.com>
2023-09-03 12:42:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,41 +2,47 @@ const Discord = require('discord.js')
 const bot = new Discord.Client()
 const client = new Discord.Client();
 const { exec } = require('child_process');
-const token = 'MTE0Mzg3MTA0MzQzMDI1NjgyMA.G_Lx6h.mi9J-Wm-hd_tGxj0DL2F0saIKoDmRL3Bf4upxY'; // Remplacez par votre propre token
+const token = ''; // Remplacez par votre propre token
 
 client.once('ready', () => {
-    console.log('Bot is ready.');
+    console.log('lancement du bot');
 });
 
 client.on('message', (message) => {
-    const channel = message.channel;
+   
 
-    let cmember = message.mentions.members.first();
     
+    if (message.mentions.users.size > 0) {
    
     if (!message.author.bot) {  // Vérifiez si le message est envoyé par un utilisateur et n'est pas envoyé par le bot lui-même
-        
-        if (message.content.toLowerCase() === 'lecky') { // Vérifiez si le contenu du message est "lecky"
-            // Répondez avec un message
-            message.channel.send('tu es qui pour @ ');
+
+        const channel = message.channel;
+
+        let cmember = message.mentions.members.first();
+
+         if(cmember.id === ''){ // identifiant de la personne concerne
+            message.channel.send('il est pas la');
+            
         }
 
-        
+        else if(cmember.id === ''){ //identifiant de la personne concerne
+            message.channel.send('lui aussi il est pas la');
 
-         if(cmember.id === 'identifiant de la personne concerné'){ //copier l'identifiant de la personne concerne pour que comme ca une fois @ le bot repond dans le channel
-            message.channel.send('tu es qui pour me @ ');
+              message.author.send('Il est trop occupé c\'est une star'); //envoie directement un message prv a la personne qui la ping 
         }
 
-        else if(cmember.id === 'identifiant de la personne concerné'){
-            message.channel.send('Il veut quoi l\'autre');
-
-            //  message.author.send('Il est trop occupé c\'est une star');
-        }
-
-        else if(cmember.id === 'identifiant de la personne concerné'){ 
+        else if(cmember.id === ''){ //identifiant de la personne concerne
             message.channel.send('un ptit cache cache allez');
         }
+        else if(cmember.id === ''){  //identifiant de la personne concerne
+            message.channel.send('C++ (javascript, raspberry, cisco) > all ');
         }
+
+
+        } else{
+            console.log("rien");
+        }
+    }
     
 });
 
